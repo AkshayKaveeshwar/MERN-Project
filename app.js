224bdb416a17abed8b994f637c584b508d8de814
AkshayKaveeshwar <akkiak95@gmail.com>
2021-02-20 20:58:10 +0530

    Adding files

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");  //this is for middleware
-const cookieParser = require("cookie-parser");  //this is for middleware
-const cors = require("cors");  //this is for middleware
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //My routes
-const authRoutes = require("./routes/auth"); //for router and here inside routes there is auth.js is there and it is own created route that is why we gave ./routes/auth
-const userRoutes = require("./routes/user"); //same as above line but here user.js
-const categoryRoutes = require("./routes/category"); //same as above line but here category.js
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
 
-//DB CONNECTION
-//In below there is DATABASE, it is actually created separate file called .env and took from that file.
-//Reason for saying this line is because .env file dont get uploaded on github/repo server usually. 
-mongoose.connect(process.env.DATABASE, {
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
-})
-.then(() => {
+  })
+  .then(() => {
     console.log("DB CONNECTED");
-});
+  });
 
 //Middlewares
-app.use(bodyParser.json());  //for middleware
-app.use(cookieParser());    //for middleware
-app.use(cors());            //for middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 //My Routes
-app.use("/api", authRoutes); //when we're running url that time /api should be there in before.
+app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
 
-//Starting a Server
+//Starting a server
 app.listen(port, () => {
-    console.log(`app is running at ${port}`);
-})
+  console.log(`app is running at ${port}`);
+});
